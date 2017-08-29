ddc2da67e0ded51748857b15af0014a43620b1ac
mattgellert <mattgellert@gmail.com>
2017-08-29 17:48:43 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,9 @@
+class Dog
+  #stuff that describes a dog object
+end
+
+fido = Dog.new
+
+snoopy = Dog.new
+
+lassie = Dog.new
---
+++ lib/person.rb
@@ -0,0 +1,7 @@
+class Person
+  #evil at their core, some are confused and think otherwise
+end
+
+adele_goldberg = Person.new
+
+alan_kay = Person.new
